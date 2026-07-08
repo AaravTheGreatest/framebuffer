651768a9ed441f36a74dff2bbb5f40886ade1d3c
ProbablyAarav <aarav25803691@gmail.com>
2026-07-08 15:27:40 -0500

    Finished implementing fb.cpp (framebuffer), time for drawing stuff!

--- src/fb.cpp
+++ src/fb.cpp
@@ -2,8 +2,17 @@
 
 Framebuffer::Framebuffer(uint32_t w, uint32_t h): width(w), height(h), pixels(w * h) {}
 Framebuffer::~Framebuffer() {}
-uint32_t Framebuffer::getWidth() const { return width; }
 
+// getters because I can't just Framebuffer.height :(
+uint32_t Framebuffer::getWidth() const { return width; }
+uint32_t Framebuffer::getHeight() const { return height; }
+uint32_t* Framebuffer::data() { return pixels.data(); }
+
+// changing dimensions (setters, because, once again, you can't just Framebuffer.height = x)
+void Framebuffer::setWidth(uint32_t w) { width = w; pixels.resize(width * height); }
+void Framebuffer::setHeight(uint32_t h) {height = h; pixels.resize(width * height); }
+
+// coloring stuff
 void Framebuffer::clear(uint32_t colour) { 
   for (size_t i = 0; i < pixels.size(); i++) pixels[i] = colour; 
 } 
